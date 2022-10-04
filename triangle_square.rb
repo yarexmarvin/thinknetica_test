7df6a7928e54c95fe09872102f880f531f8c91c4
Yarex Marvin <yarexmarvin@yahoo.com>
2022-10-04 18:49:21 +0400

    Triangle square

---
+++ triangle_square.rb
@@ -0,0 +1,13 @@
+
+
+puts 'Enter the triangle base'
+
+base = gets.chomp.to_i
+
+puts 'Enter the triangle height'
+
+height = gets.chomp.to_i
+
+square = 0.5 * base * height
+
+puts "Triangle square = #{square}"
